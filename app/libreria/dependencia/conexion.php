d8e478833092e6e03813d495c0d83c40c469a7bd
INGJEISSON <jibarguen@suyo.co>
2020-05-10 10:17:59 -0500

    Conectarse

--- app/libreria/dependencia/conexion.php
+++ app/libreria/dependencia/conexion.php
@@ -3,11 +3,12 @@
 $host="avilldb.ceenrslgtl8b.us-east-2.rds.amazonaws.com";
 $user="kjvqxxdyioocpd";	
 $pass="1ecfa73d2788743c43ddba2afff01f39cac73a5b50d259819252a1408ca5dcd1";	
-$dbname="d4ppp5t71v3odv";	
-$conexion = pg_connect("host=$host dbname=$dbname user=$user password=$pass");
+$dbname="d4ppp5t71v3odv";
+$port=5432;
+$conexion = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
 
-/*if($conexion)
+if($conexion)
 echo "<br>conectado";
 else
-echo "<br>No conectado";*/
+echo "<br>No conectado";
 ?>
