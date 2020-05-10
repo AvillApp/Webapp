786a6bb2d559b133a2702a0e887c1cfc1a467472
INGJEISSON <jibarguen@suyo.co>
2020-05-09 22:35:10 -0500

    Se acomoda estructura de solicitudees

--- app/view/perfil/solicitudes.php
+++ app/view/perfil/solicitudes.php
@@ -1,63 +1,79 @@
 <?php
 @include('../../config.php');
 
-$sql2 = "select pedidos.telealt, pedidos.id_user, pedidos.indicacion, pedidos.direccion, users.nombre
- from pedidos, users where users.id=pedidos.id_user and pedidos.estado=1";
+$sql2 = "select pedidos.telealt, pedidos.id_user, pedidos.indicacion, pedidos.direccion, users.nombre, pedidos.fecha_registro
+ from pedidos, users where users.id=pedidos.id_user and pedidos.estado=1 order by fecha_registro";
 $query2 = pg_query($conexion, $sql2);
 $rows2 = pg_num_rows($query2);
 
-    if($rows2){
-        while($datos2=pg_)
 ?>
-<div class="row">
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+<?php
 
-<!-- Content Column -->
-<div class="col-lg-6 mb-4">
 
-  <!-- Project Card Example -->
-  <div class="card shadow mb-4">
-    <div class="card-header py-3 success bg-light">
-        <!-- <h6 class="m-0 font-weight-bold text-white">Nuevo viaje</h6> -->
-        <div align='center'>
-            <button class='btn btn-success'>Aceptar </button>
-            <button class='btn btn-primary'>Buscar conrudctor</button>
-            <button class='btn btn-danger'>Rechazar</button>
-        </div>
+    //if($rows2){
      
-    </div>
-    <div class='row'>
-        <div class="card-body">
-            <p><b>Dirección: </b><?php echo $datos2['direccion'] ?></p>
-            <p><b>Indicación:</b><?php echo $datos2['indicacion'] ?> </p>
-            <p><b>Teléfono:</b><?php echo $datos2['telealt'] ?> </p>
-            <p><b>Nombre:</b><?php echo $datos2['nombre'] ?> </p>                    
-        </div>
-    </div>
-    
-  </div>
+  ?>
+          <div class="row">
 
-</div>
+          <!-- Content Column -->
+          <div class="col-lg-12 mb-12">
 
-<div class="col-lg-6 mb-4">
+            <!-- Project Card Example -->
+            <div class="card shadow mb-4 lg-12">
+                <table id="example" class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Dirección</th>
+                            <th>Indicación</th>
+                            <th>Teléfono</th>
+                            <th>Cliente</th>
+                            <th>Fecha de registro</th>
+                            <th>Estado</th>
+                            <th>Aceptar</th>
+                            <th>Conductor</th>
+                            <th>Rechazar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php 
+                    $i=1;
+                    while($datos2=pg_fetch_assoc($query2)){
+                    ?>
+                        <tr>
+                          <td><?php echo $i; ?> </td>
+                          <td><?php echo $datos2['direccion'] ?></td>
+                          <td><?php echo $datos2['indicacion'] ?> </td>
+                          <td><?php echo $datos2['telealt'] ?></td>
+                          <td><?php echo $datos2['nombre'] ?> </td>
+                          <td><?php echo $datos2['fecha_registro'] ?></td>
+                          <td><?php echo $datos2['nombre'] ?> </td>                         
+                          <td><button class='btn btn-success' id='aceptar<?php echo $i ?>'>Aceptar</button> </td>
+                          <td><button class='btn btn-primary'>Conductor</button> </td>
+                          <td><button class='btn btn-danger'>Rechazar</button></td>
+                        </tr>
+                        <script>
+                              $("#aceptar<?php echo $i ?>").click(function(){
+                                  alert("Bien<?php echo $i ?>")
+                              });
+                            </script>
+                        <?php
+                        $i++;
+                      } 
+                      
+                      ?>
+                    </tbody>
+                </table>
+              
+            </div>
 
+          </div>
+          
 
-  <!-- Approach -->
-  <div class="card shadow mb-4">
-    <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary">Aplicar filtros</h6>
-    </div>
-    <div class="card-body">
-      <p>Conductor</p>
-      <select class='form-control'>
-        <option></option>
-      </select>
-      <p><button class='btn btn-success'>Filtrar</button></p>
-      <p class="mb-0">Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classes.</p>
-    </div>
-  </div>
-
-</div>
-</div>
-    <?php }else
-    echo "No hay solicitudes del cliente";
-    ?>
+<script>
+$(document).ready(function() {
+    $('#example').DataTable();
+} );
+</script>
