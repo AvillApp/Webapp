1ead021c60d720f4aef713bbc1d2d2536210926b
INGJEISSON <jibarguen@suyo.co>
2020-05-10 10:06:46 -0500

    Cambiando parámetros de conexión AWS

--- app/libreria/dependencia/conexion.php
+++ app/libreria/dependencia/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$host="ec2-50-17-90-177.compute-1.amazonaws.com";
+$host="avilldb.ceenrslgtl8b.us-east-2.rds.amazonaws.com";
 $user="kjvqxxdyioocpd";	
 $pass="1ecfa73d2788743c43ddba2afff01f39cac73a5b50d259819252a1408ca5dcd1";	
 $dbname="d4ppp5t71v3odv";	
