41dc9ed10b653b27d812b226d10f08c5c8a92031
INGJEISSON <jibarguen@suyo.co>
2020-05-10 09:44:46 -0500

    cambiar forma del protocolo

--- app/routers/aplication.php
+++ app/routers/aplication.php
@@ -1,8 +1,19 @@
 <?php 
 $app = 'intranet';
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 
+if (isset($_SERVER['HTTPS']) &&
+    ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+  $protocolo= 'https://';
+}
+else {
+  $protocolo = 'http://';
+}
 //$protocolo = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
-$protocolo = stripos($_SERVER['SERVER_PROTOCOL'],'https') === 0 ? 'https://' : 'http://';
+//$protocolo = stripos($_SERVER['SERVER_PROTOCOL'],'https') === 0 ? 'https://' : 'http://';
 
 //$domain = $_SERVER['HTTP_HOST'];
 //$protocolo='https://';
