a8ed6a75ebab2a97f47430e38ba37b1c62e10e20
INGJEISSON <jibarguen@suyo.co>
2020-06-07 17:52:10 -0500

    Pruebas

--- desc/mobile/index.php
+++ desc/mobile/index.php
@@ -1,6 +1,4 @@
 Versión 1.0;
-
-En proceso de actualización
 <script>
 parent.location='https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40jeisson/avillApp-428e0b48136845d58a919f8cda36c137-signed.apk';
 </script>
