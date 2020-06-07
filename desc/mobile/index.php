86c2523245629520fd4d12da4100c4e357432fb4
INGJEISSON <jibarguen@suyo.co>
2020-06-07 17:48:36 -0500

    Pruebas

--- desc/mobile/index.php
+++ desc/mobile/index.php
@@ -2,5 +2,5 @@ Versión 1.0;
 
 En proceso de actualización
 <script>
-//parent.location='';
+parent.location='https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40jeisson/avillApp-428e0b48136845d58a919f8cda36c137-signed.apk';
 </script>
