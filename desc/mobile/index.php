c7dfdd693af6d22a83e0bbd0a416a3c8d671a6e2
INGJEISSON <jibarguen@suyo.co>
2020-06-08 12:07:35 -0500

    Cambio de aplicación

--- desc/mobile/index.php
+++ desc/mobile/index.php
@@ -1,4 +1,4 @@
 Versión 1.0;
 <script>
-parent.location='https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40jeisson/avillApp-428e0b48136845d58a919f8cda36c137-signed.apk';
+parent.location='https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40jeisson/avillApp-09a90e2ab4334ddbbf110eb0c6f4715d-signed.apk';
 </script>
