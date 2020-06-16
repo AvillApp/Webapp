d0ee95c491e01ffd790baaa24138945a1144383a
JEISSON (SUYO) <jibarguen@suyo.co>
2020-06-16 09:59:27 -0500

    Notificación push

--- desc/mobile/index.php
+++ desc/mobile/index.php
@@ -1,4 +1,4 @@
 Versión 1.0;
 <script>
-parent.location='https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40jeisson/avillApp-99f4c288cfbf4f268c0e5b4762af1a27-signed.apk';
+parent.location='https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40jeisson/avillApp-1b38633cb2854e8eb606c1b17c8674e6-signed.apk';
 </script>
