00478a442de1c3b781c04a7b2bc0adb6bf7ee889
JEISSON (SUYO) <jibarguen@suyo.co>
2020-06-16 18:52:20 -0500

    actualizacion app

--- desc/mobile/index.php
+++ desc/mobile/index.php
@@ -1,4 +1,4 @@
 Versión 1.0;
 <script>
-parent.location='https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40jeisson/avillApp-1b38633cb2854e8eb606c1b17c8674e6-signed.apk';
+parent.location='https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/android/%40jeisson/avillApp-7aab740f2d9e4d06a8d2d6a271a0b898-signed.apk';
 </script>
