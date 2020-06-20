7ac50da1faca34f5c4c7f8639275ddcbc6730ec3
JEISSON (SUYO) <jibarguen@suyo.co>
2020-06-20 07:13:49 -0500

    SUbir foto de conductor y envio de SMS

--- app/view/perfil/solicitudes.php
+++ app/view/perfil/solicitudes.php
@@ -1,7 +1,7 @@
 <?php
 @include('../../config.php');
 
-$sql2 = "select pedidos.token, users.telefono, pedidos.vehiculo_usu, pedidos.emision, pedidos.id, pedidos.latitude, pedidos.longitude, estado.id as idestado, pedidos.fecha_update, pedidos.telealt, pedidos.id_user,
+$sql2 = "select pedidos.token, pedidos.vehiculo_usu, pedidos.emision, pedidos.id, pedidos.latitude, pedidos.longitude, estado.id as idestado, pedidos.fecha_update, pedidos.telealt, pedidos.id_user,
  pedidos.indicacion, pedidos.direccion, users.nombre, pedidos.fecha_registro, estado.descripcion as estado, users.telefono
  from pedidos, users,estado where estado.id=pedidos.estado and users.id=pedidos.id_user and (pedidos.estado!=6) order by pedidos.id desc";
 $query2 = pg_query($conexion, $sql2);
@@ -87,7 +87,7 @@ $_SESSION['solic'] = $rows2;
                                  // alert("Bien<?php echo $i ?>")
                                   $("#solicitudModal").modal();
                                   $("#contenido").empty();
-                                  $("#contenido").load('aceptar.php?id_pedido=<?php echo $datos2['id'] ?>&tel_usu=<?php echo base64_encode($datos['telefono']) ?>&token=<?php echo base64_encode($datos2['token']) ?>');
+                                  $("#contenido").load('aceptar.php?id_pedido=<?php echo $datos2['id'] ?>&tel_usu=<?php echo base64_encode($datos2['telefono']) ?>&token=<?php echo base64_encode($datos2['token']) ?>');
                                   $("#contenido").show();
                               });
                               
