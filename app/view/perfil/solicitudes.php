3b2a4e696e9f6cb7ac08c0888b237ef1a524faa7
JEISSON (SUYO) <jibarguen@suyo.co>
2020-06-20 07:12:12 -0500

    SUbir foto de conductor y envio de SMS

--- app/view/perfil/solicitudes.php
+++ app/view/perfil/solicitudes.php
@@ -1,7 +1,7 @@
 <?php
 @include('../../config.php');
 
-$sql2 = "select pedidos.token, pedidos.vehiculo_usu, pedidos.emision, pedidos.id, pedidos.latitude, pedidos.longitude, estado.id as idestado, pedidos.fecha_update, pedidos.telealt, pedidos.id_user,
+$sql2 = "select pedidos.token, users.telefono, pedidos.vehiculo_usu, pedidos.emision, pedidos.id, pedidos.latitude, pedidos.longitude, estado.id as idestado, pedidos.fecha_update, pedidos.telealt, pedidos.id_user,
  pedidos.indicacion, pedidos.direccion, users.nombre, pedidos.fecha_registro, estado.descripcion as estado, users.telefono
  from pedidos, users,estado where estado.id=pedidos.estado and users.id=pedidos.id_user and (pedidos.estado!=6) order by pedidos.id desc";
 $query2 = pg_query($conexion, $sql2);
